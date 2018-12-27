e7d80e8e7a6b179f81dc5707da2fbe7f358ef6a1
panbak <pan22bak@gmail.com>
2018-12-27 19:17:49 +0200

    added daily_appointment and timeslot model. Also migrations for tables daily_appointments and timeslots

---
+++ database/migrations/2018_12_27_164630_create_timeslots_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimeslotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timeslots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user_id');
+            $table->integer('daily_appointments_id');
+            $table->integer('ticket_num')->nullable();
+            $table->timestamp('slot')->nullable()->default(null);
+            $table->text('comment')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timeslots');
+    }
+}
---
+++ database/migrations/2018_12_27_170818_create_daily_appointments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDailyAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('daily_appointments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('appointment_id');
+            $table->timestamp('start')->nullable()->default(null);
+            $table->timestamp('end')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('daily_appointments');
+    }
+}
