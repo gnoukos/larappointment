70934f50adf5aecbcc44f700a5953b4731c335dd
panbak <pan22bak@gmail.com>
2019-02-26 17:01:14 +0200

    show first available date and show next fix

--- resources/views/pages/datepicker.blade.php
+++ resources/views/pages/datepicker.blade.php
@@ -112,43 +112,70 @@
 
         $("#makeAppointmentButton").hide(); //hide button until hour selected
 
-        $('.calendar').pignoseCalendar({ //initialize pignose calendar
-            initialize: false,
-            disabledDates: disabledDatesArray,
-            minDate: moment().format("YYYY-MM-DD"), //disable dates before today
-            maxDate: maxAvailDate, //disable dates after a certain date
-            select: function(date, context) { //callback function to get selected date
-                try {
-                    $("#hourButtonContainer").empty();
-                    console.log(date[0]._i); //selected date
-                    var option = getUrlParameter('option');
-                    getFreeTimeslots(option, date[0]._i);
-                    $("#makeAppointmentButton").hide();
-                    $("#chooseDatePrompt").hide();
-                }catch (e) {
-                    $("#makeAppointmentButton").hide();
-                    $("#chooseDatePrompt").show();
+
+
+        /////SET FIRST AVAILABLE DATE//////
+        var maxDateObj = new Date(maxAvailDate); //creates max available date object to use in for loop
+        var availDate = new Date(moment().format("YYYY-MM-DD"));
+        var d = availDate;
+        while (true){
+
+            var isAvail = true;
+
+            $.each( disabledDatesArray, function( key, value ) { //foreach loop in disabled dates array
+                //console.log( key + ": " + value );
+                var tmpDate = new Date(value); // creates temporary date object for disabled date
+                if(d.getTime() == tmpDate.getTime()){ // checks if current date object equals disabled date object
+                    isAvail = false; // if true sets isAvail to false, because date seems to be unavailable and the loop breaks
+                    return false;
                 }
+                availDate = d; //just gets the last date of for loop
+            });
+
+            if(isAvail===true){ //if the last date was found to be available the loop stops
+                $('.calendar').pignoseCalendar({ //initialize pignose calendar
+                    initialize: true,
+                    date: moment(availDate.getTime()),
+                    disabledDates: disabledDatesArray,
+                    minDate: moment().format("YYYY-MM-DD"), //disable dates before today
+                    maxDate: maxAvailDate, //disable dates after a certain date
+                    select: function(date, context) { //callback function to get selected date
+                        try {
+                            $("#hourButtonContainer").empty();
+                            console.log(date[0]._i); //selected date
+                            var option = getUrlParameter('option');
+                            getFreeTimeslots(option, date[0]._i);
+                            $("#makeAppointmentButton").hide();
+                            $("#chooseDatePrompt").hide();
+                        }catch (e) {
+                            $("#makeAppointmentButton").hide();
+                            $("#chooseDatePrompt").show();
+                        }
+                    }
+                });
+                $('[data-date='+moment(availDate.getTime()).format("YYYY-MM-DD")+']').click();
+                $('[data-date='+moment(availDate.getTime()).format("YYYY-MM-DD")+']').click();
+                $('[data-date='+moment(availDate.getTime()).format("YYYY-MM-DD")+']').click();
+                break;
             }
-        });
 
+            d.setDate(d.getDate() + 1);//next possible available date
 
-        var availDate = new Date(moment().format("YYYY-MM-DD"));//just a temporary object that will store the first available date found
+            if(d.getTime() > maxDateObj.getTime()){
+                console.log("Something went really wrong!");
+                break;
+            }
+        }
+        /////SET FIRST AVAILABLE DATE END//////
 
-        var nextAvailableDateButtonClicks = 0; //counter to check how many times the next available date button is clicked
-
+        /////////SHOW NEXT AVAILABLE DATE//////////
         $("#nextAvailableDateButton").click(function(){	//function to find first available date
 
-            nextAvailableDateButtonClicks++;
-
-            if(nextAvailableDateButtonClicks>1){ //if its not the first time it means that the loop should continue from the exact next date of last available
-                availDate.setDate(availDate.getDate() + 1);
-            }
-
             var maxDateObj = new Date(maxAvailDate); //creates max available date object to use in for loop
-            var d = new Date();
 
-            for (d = availDate; d.getTime() <= maxDateObj.getTime(); d.setDate(d.getDate() + 1)) { // for loop from today until max available date
+            availDate.setDate(availDate.getDate() + 1);
+
+            for (var d = availDate; d.getTime() <= maxDateObj.getTime(); d.setDate(d.getDate() + 1)) { // for loop from today until max available date
 
                 var isAvail = true;
 
@@ -166,11 +193,11 @@
                     break;
                 }
             }
-            //$('.calendar').pignoseCalendar('set', moment(availDate.getTime()).format("YYYY-MM-DD")); //sets the available date to the pignose calendar
             $('[data-date='+moment(availDate.getTime()).format("YYYY-MM-DD")+']').click();
             console.log( $('[data-date='+moment(availDate.getTime()).format("YYYY-MM-DD")+']'));
-
         });
+        /////////SHOW NEXT AVAILABLE DATE END //////////
+
 
         function getFreeTimeslots(option, date){
             console.log(option+ "   " + date);
