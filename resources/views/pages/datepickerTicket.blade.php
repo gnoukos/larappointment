df112b738677a662fed585b0616187264d24fef5
panbak <pan22bak@gmail.com>
2019-02-27 10:13:54 +0200

    FIX for ticket datepicker go to new month when click next date

--- resources/views/pages/datepickerTicket.blade.php
+++ resources/views/pages/datepickerTicket.blade.php
@@ -62,7 +62,7 @@
 
         $("#selectedDate").hide();
 
-        $('.calendar').pignoseCalendar({ //initialize pignose calendar
+        /*$('.calendar').pignoseCalendar({ //initialize pignose calendar
             initialize: false,
             disabledDates: disabledDatesArray,
             minDate: moment().format("YYYY-MM-DD"), //disable dates before today
@@ -82,7 +82,103 @@
                     $("#selectedDate").hide();
                 }
             }
+        });*/
+
+
+        /////SET FIRST AVAILABLE DATE//////
+        var maxDateObj = new Date(maxAvailDate); //creates max available date object to use in for loop
+        var availDate = new Date(moment().format("YYYY-MM-DD"));
+        var d = availDate;
+        while (true){
+
+            var isAvail = true;
+
+            $.each( disabledDatesArray, function( key, value ) { //foreach loop in disabled dates array
+                //console.log( key + ": " + value );
+                var tmpDate = new Date(value); // creates temporary date object for disabled date
+                if(d.getTime() == tmpDate.getTime()){ // checks if current date object equals disabled date object
+                    isAvail = false; // if true sets isAvail to false, because date seems to be unavailable and the loop breaks
+                    return false;
+                }
+                availDate = d; //just gets the last date of for loop
+            });
+
+            if(isAvail===true){ //if the last date was found to be available the loop stops
+                $('.calendar').pignoseCalendar({ //initialize pignose calendar
+                    initialize: true,
+                    date: moment(availDate.getTime()),
+                    disabledDates: disabledDatesArray,
+                    minDate: moment().format("YYYY-MM-DD"), //disable dates before today
+                    maxDate: maxAvailDate, //disable dates after a certain date
+                    select: function(date, context) { //callback function to get selected date
+                        try {
+                            $('#selectedDate #date').empty();
+                            console.log(date[0]._i); //selected date
+                            $("#makeAppointmentButton").show();
+                            $("#chooseDatePrompt").hide();
+                            var option = getUrlParameter('option');
+                            getDailyAppointment(option, date[0]._i);
+                        }catch (e) {
+                            $("#makeAppointmentButton").hide();
+                            $("#chooseDatePrompt").show();
+                            $('#selectedDate #date').empty();
+                            $("#selectedDate").hide();
+                        }
+                    }
+                });
+                $('[data-date='+moment(availDate.getTime()).format("YYYY-MM-DD")+']').click();
+                $('[data-date='+moment(availDate.getTime()).format("YYYY-MM-DD")+']').click();
+                $('[data-date='+moment(availDate.getTime()).format("YYYY-MM-DD")+']').click();
+                break;
+            }
+
+            d.setDate(d.getDate() + 1);//next possible available date
+
+            if(d.getTime() > maxDateObj.getTime()){
+                console.log("Something went really wrong!");
+                break;
+            }
+        }
+        /////SET FIRST AVAILABLE DATE END//////
+
+        /////////SHOW NEXT AVAILABLE DATE//////////
+        $("#nextAvailableDateButton").click(function(){	//function to find first available date
+
+            var maxDateObj = new Date(maxAvailDate); //creates max available date object to use in for loop
+
+            var preMonth = availDate.getMonth();
+
+            availDate.setDate(availDate.getDate() + 1);
+
+            var nextMonth = availDate.getMonth();
+
+            if(preMonth !== nextMonth){
+                $('.pignose-calendar-top-next').click();
+            }
+
+            for (var d = availDate; d.getTime() <= maxDateObj.getTime(); d.setDate(d.getDate() + 1)) { // for loop from today until max available date
+
+                var isAvail = true;
+
+                $.each( disabledDatesArray, function( key, value ) { //foreach loop in disabled dates array
+                    //console.log( key + ": " + value );
+                    var tmpDate = new Date(value); // creates temporary date object for disabled date
+                    if(d.getTime() == tmpDate.getTime()){ // checks if current date object equals disabled date object
+                        isAvail = false; // if true sets isAvail to false, because date seems to be unavailable and the loop breaks
+                        return false;
+                    }
+                    availDate = d; //just gets the last date of for loop
+                });
+
+                if(isAvail==true){ //if the last date was found to be available the loop stops
+                    break;
+                }
+            }
+            $('[data-date='+moment(availDate.getTime()).format("YYYY-MM-DD")+']').click();
+            //console.log( $('[data-date='+moment(availDate.getTime()).format("YYYY-MM-DD")+']'));
         });
+        /////////SHOW NEXT AVAILABLE DATE END //////////!!!
+
 
         function getDailyAppointment(option, date){
             console.log(option+ "   " + date);
@@ -137,7 +233,7 @@
             }
         };
 
-        var availDate = new Date(moment().format("YYYY-MM-DD"));//just a temporary object that will store the first available date found
+        /*var availDate = new Date(moment().format("YYYY-MM-DD"));//just a temporary object that will store the first available date found
 
         var nextAvailableDateButtonClicks = 0; //counter to check how many times the next available date button is clicked
 
@@ -171,7 +267,7 @@
                 }
             }
             $('.calendar').pignoseCalendar('set', moment(availDate.getTime()).format("YYYY-MM-DD")); //sets the available date to the pignose calendar
-        });
+        });*/
 
     </script>
 
