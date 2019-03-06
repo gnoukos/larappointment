04faf9cd74995ed45129e5ecf51eb456f9e9c5a0
George Noukos <toketog@gmail.com>
2019-03-06 16:44:23 +0200

    added credits

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,28 +1,8 @@
 <footer class="pt-4 my-md-5 pt-md-5">
-    <div class="row ">
-{{--        <div class="col-12 col-md">
-            <small class="d-block mb-3 text-light">&copy; 2017-2018</small>
+    <hr />
+    <div class="row text-center">
+        <div class="col-md-12 textw">
+            Developed by <a href="https://www.linkedin.com/in/george-noukos-03835bab/">George Raphael Noukos</a> and <a href="https://www.linkedin.com/in/panagiotis-bakas-619353116/">Panagiotis Bakas</a>. Supervised by <a href="https://arch.icte.uowm.gr"> Minas Dasygenis</a>
         </div>
-        <div class="col-6 col-md">
-            <h5>Features</h5>
-            <ul class="list-unstyled text-small ">
-                <li><a class="text-light" href="#">Admin</a></li>
-                <li><a class="text-light" href="#">Client</a></li>
-            </ul>
-        </div>
-        <div class="col-6 col-md">
-            <h5>Documentation</h5>
-            <ul class="list-unstyled text-small">
-                <li><a class="text-light" href="#">PDF</a></li>
-                <li><a class="text-light" href="#">Web</a></li>
-            </ul>
-        </div>
-        <div class="col-6 col-md">
-            <h5>About</h5>
-            <ul class="list-unstyled text-small">
-                <li><a class="text-light" href="#">Team</a></li>
-                <li><a class="text-light" href="#">License</a></li>
-            </ul>
-        </div>--}}
     </div>
 </footer>
