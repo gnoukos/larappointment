74deb5cb057d25a60383e070f357cc2195fe5bc7
George Noukos <toketog@gmail.com>
2019-03-06 16:44:49 +0200

    added credits

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,7 +2,7 @@
     <hr />
     <div class="row text-center">
         <div class="col-md-12 textw">
-            Developed by <a href="https://www.linkedin.com/in/george-noukos-03835bab/">George Raphael Noukos</a> and <a href="https://www.linkedin.com/in/panagiotis-bakas-619353116/">Panagiotis Bakas</a>. Supervised by <a href="https://arch.icte.uowm.gr"> Minas Dasygenis</a>
+            Developed by <a href="https://www.linkedin.com/in/george-noukos-03835bab/">George Raphael Noukos</a> and <a href="https://www.linkedin.com/in/panagiotis-bakas-619353116/">Panagiotis Bakas</a>. Supervised by <a href="https://arch.icte.uowm.gr/mdasyg"> Minas Dasygenis</a>
         </div>
     </div>
 </footer>
